cf03f066de6e9be35a69abcad61eaf2acc24bde8
Joao Vale <jcarlos.vale@gmail.com>
2022-05-05 20:15:39 +0200

    initial commit

---
+++ src/test/java/br/com/letscode/HelloWorldApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.letscode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloWorldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
